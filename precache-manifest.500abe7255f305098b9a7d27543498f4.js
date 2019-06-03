0071802024f4131b80bf096bfd32a762a9d80e34
YaUsatiy <vanek17121999@gmail.com>
2019-06-04 00:50:48 +0300

    Updates

---
+++ precache-manifest.500abe7255f305098b9a7d27543498f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38b60ee13ae4f22bdd4f",
+    "url": "/NewsAPINew/static/js/runtime~main.56b27557.js"
+  },
+  {
+    "revision": "bb4e0698d2dbcfb376c9",
+    "url": "/NewsAPINew/static/js/main.c3e22546.chunk.js"
+  },
+  {
+    "revision": "676f2a6f6495ad3726d3",
+    "url": "/NewsAPINew/static/js/2.4d8938e8.chunk.js"
+  },
+  {
+    "revision": "bb4e0698d2dbcfb376c9",
+    "url": "/NewsAPINew/static/css/main.82aed2ed.chunk.css"
+  },
+  {
+    "revision": "8e5a58c5d712c429c010f6d8535915f5",
+    "url": "/NewsAPINew/index.html"
+  }
+];
